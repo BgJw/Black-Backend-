e31e00b849773da9672728bac79b2db34b95d12a
Bohdan Jejnikow <oskarstalker@gmail.com>
2024-04-08 18:46:07 +0200

    Update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -9,6 +10,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Разрешенные методы
     allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
   });
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
