0ea4de75815119d6435b139dd88d8636d47b567a
Bohdan Jejnikow <oskarstalker@gmail.com>
2024-06-23 18:19:30 +0200

    add correct cors url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://black-frontend-production.up.railway.app'],
+    origin: ['http://localhost:3000', 'https://black-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
